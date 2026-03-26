c43a92523ea7d6593ed5e3044acccbe7195f8f4f
hankaibo <hankaibo@hotmail.com>
2026-03-26 22:09:06 +0800

    format: 格式化代码

--- src/routes/(admin)/user/data-table.svelte
+++ src/routes/(admin)/user/data-table.svelte
@@ -157,7 +157,7 @@
 		</Table.Root>
 	</div>
 	<div class="flex items-center justify-end space-x-2 py-4">
-		<div class="text-muted-foreground flex-1 text-sm">
+		<div class="flex-1 text-sm text-muted-foreground">
 			{table.getFilteredSelectedRowModel().rows.length} / {table.getFilteredRowModel().rows.length}
 		</div>
 		<Button
